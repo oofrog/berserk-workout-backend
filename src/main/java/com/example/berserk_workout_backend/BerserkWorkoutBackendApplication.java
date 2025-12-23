e91adebaaeb3360179a8fa8f92ddcea21d14b28c
oofrog <oofrog0823@gmail.com>
2025-12-23 17:42:24 +0900

    spring init

---
+++ src/main/java/com/example/berserk_workout_backend/BerserkWorkoutBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.berserk_workout_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerserkWorkoutBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerserkWorkoutBackendApplication.class, args);
+	}
+
+}
